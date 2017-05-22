5aed73b38a66287f37d38fdaad787058d362da00
Adroso <adrian.lapico@my.jcu.edu.au>
2017-05-22 23:10:28 +1000

    Created Regeneration Functionality in health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
     public int startingHelath = 100;
     public int currentHealth;
+    public int regeneration;
     public Slider healthSlider;
     public Image damageImage;
     public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    private float timestamp = 0f; //delays regen
+    private float regenDelayTS = 0f; //delays regening health all at once
+
 
     Animator anim;
     AudioSource playerAudio;
@@ -19,38 +24,49 @@ public class PlayerHealth : MonoBehaviour {
     bool isDead;
     bool damaged;
 
-	// Use this for initialization
-	void Awake()
+    // Use this for initialization
+    void Awake()
     {
         anim = GetComponent<Animator>();
         playerAudio = GetComponent<AudioSource>();
         playerMovement = GetComponent<PlayerMovement>();
         currentHealth = startingHelath;
     }
-		
-	
-	// Update is called once per frame
-	void Update () {
-    if (damaged)
+
+
+    // Update is called once per frame
+    void Update()
     {
+        if (currentHealth < startingHelath && (Time.time > (timestamp + 10.0f))) //10 seconds after no damage
+        {
+            currentHealth += regeneration;
+            healthSlider.value = currentHealth;
+            timestamp = 0f;
+        }
+
+        if (damaged)
+        {
             damageImage.color = flashColour;
-  
-    }
+            
+        }
         else
         {
             damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+
         }
+
         damaged = false;
-	}
+    }
 
     public void TakeDamage(int amount)
     {
         damaged = true;
         currentHealth -= amount;
         healthSlider.value = currentHealth;
+        timestamp = Time.time; // for regen
         //play hurt audio
 
-        if(currentHealth <=0 && !isDead)
+        if (currentHealth <= 0 && !isDead)
         {
             Death();
         }
