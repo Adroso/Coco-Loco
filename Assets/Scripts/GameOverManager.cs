5914841007abebeafc3c17ea5951161405318ad1
Adroso <adrian.lapico@my.jcu.edu.au>
2017-05-22 21:58:23 +1000

    Created Script to handle playing game over screen

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GameOverManager : MonoBehaviour {
 
+    public PlayerHealth playerHealth;
+    Animator anim;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+
+        anim = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+        }
 		
 	}
 }
