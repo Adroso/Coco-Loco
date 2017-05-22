0e7bdcba24e24a2f90f15788cc66243c3683770e
Luke <luke.schaumburg@my.jcu.edu.au>
2017-05-22 16:24:15 +1000

    set up enemy attack and health scripts

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    public float attackSpeed = 1.0f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+    // Use this for initialization
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is in range.
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        // If the exiting collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is no longer in range.
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= attackSpeed && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if (PlayerHealth.currentHealth > 0)
+        {
+            PlayerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
